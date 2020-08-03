83464fc1c01c4f2fab644670ef2d5bf0c6c90c8c
José Eduardo <josee.edumont@gmail.com>
2020-08-03 20:43:07 -0300

    Inital commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Todos.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :todos,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+end
