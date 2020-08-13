4c7158460e90711973cb5181a9b6f8494e3b6b0c
José Eduardo <josee.edumont@gmail.com>
2020-08-13 17:58:41 -0300

    Added supervisor to run server

---
+++ lib/todos/application.ex
@@ -0,0 +1,15 @@
+defmodule Todos.Application do
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: Todos, options: [port: 4001]}
+    ]
+
+    opts = [strategy: :one_for_one, name: Todos.Supervisor]
+
+    Logger.info("Starting application...")
+    Supervisor.start_link(children, opts)
+  end
+end
